73aab2fe27174157a2445acbf006dc42d397d960
Yusuf Ahmad <yusufahmad0350@mail.com>
2023-02-20 17:45:17 +0000

    [Application] Add Model Framework

---
+++ Application.py
@@ -0,0 +1,16 @@
+class Application:
+    def __init__(self):
+        self.state = "Initializing"
+        self.weather_data = None
+
+    def read_data(self):
+        self.state = "Reading data from the weather data system"
+        print(self.state)
+        # simulate reading data from sensors
+        #self.weather_data = {"temperature": 75.0, "humidity": 70.0, "pressure": 1013.0}
+
+    def store_data(self):
+        self.state = "Storing the data"
+        print(self.state)
+        # simulate storing data in a database
+        print("Storing weather data:", self.weather_data)
